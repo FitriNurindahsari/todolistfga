ca3ed5bfd4a66809d025731c8ed6246aea9c5a84
FitriNurindahsari <fitrinurindahsari26@gmail.com>
2022-06-24 10:18:41 +0700

    Project To Do List React JS Finish

---
+++ src/components/UpdateForm.jsx
@@ -0,0 +1,25 @@
+const UpdateForm = ({ updateData,changeTask, updateTask, cancelUpdate }) => {
+    return(
+        <>
+        <div className="row">
+            <div className="col">
+            <input 
+            value={updateData && updateData.title}
+            onChange={ (e) => changeTask(e)}
+            className="form-control form-control-lg"/>
+            </div>
+            <div className="col-auto">
+            <button
+                onClick={updateTask}
+                className="btn btn-lg btn-primary mr-20">Update</button>
+            <button 
+                onClick={cancelUpdate}
+                className="btn btn-lg btn-danger">Cancel</button>
+            </div>
+        </div>
+        <br/>
+        </>
+    )
+}
+
+export default UpdateForm;
